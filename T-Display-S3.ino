552d028217033786ab21616f5b1873e71a9f2a68
helioz2000 <helioz2000@gmail.com>
2022-10-10 16:59:27 +1100

    1st commit

---
+++ T-Display-S3.ino
@@ -0,0 +1,20 @@
+/*
+Example and test project for L21D T-Display-S3 Development board
+The board has an ESP32-S3R8 Dual-code LX7 nad includes a 1.9" ST7789V IPSTFT LCD (170x320 RGB)
+
+Selection
+Board: ESP32S3 Dev Module
+
+The display module uses a parallel interface, not SPI. So the TFT_eSPI library is of no use.
+
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
